5667405ae5b3899b22d62b7ff3edbabf1221ad21
Sudipta Biswas <sudiptabiswas.ss6295@gmail.com>
2026-07-05 10:32:36 +0530

    Time: 3 ms (55.97%), Space: 13.3 MB (36.13%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        hash_map = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in hash_map:
+                return [hash_map[complement],i]
+
+            hash_map[nums[i]] = i    
